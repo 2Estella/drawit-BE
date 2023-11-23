00c142c9abec8f401e7e24dc2df2106594b2180f
2Estella <96452250+2Estella@users.noreply.github.com>
2023-11-23 17:58:10 +0900

    feat: Logger 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const appLogger = new Logger('AppLogger');
+  appLogger.debug('This is a debug log.');
 
   app.enableCors();
 
