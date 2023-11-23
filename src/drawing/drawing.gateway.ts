03210ff64496b7fd28824e32527d2c6bb4339139
2Estella <96452250+2Estella@users.noreply.github.com>
2023-11-23 17:59:25 +0900

    feat: room 생성 작업 (진행중)

--- src/drawing/drawing.gateway.ts
+++ src/drawing/drawing.gateway.ts
@@ -4,8 +4,7 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   // OnGatewayInit,
-  SubscribeMessage,
-  MessageBody
+  SubscribeMessage
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { DrawingService } from './drawing.service';
@@ -19,40 +18,88 @@ export class DrawingGateway implements OnGatewayConnection, OnGatewayDisconnect 
   server: Server;
 
   handleConnection(client: Socket) {
-    this.drawingService.handleConnection(client);
+    client.leave(client.id);
+    client.data.roomId = 'room:lobby';
+    client.join('room:lobby');
   }
 
   handleDisconnect(client: Socket) {
-    this.drawingService.handleDisconnect(client);
+    const { roomId } = client.data;
+    if (!this.server.sockets.adapter.rooms.get(roomId)) {
+      this.drawingService.deleteRoom(roomId);
+      this.server.emit('getChatRoomList', this.drawingService.getRoomList());
+    }
+    console.log('disconnected : ', client.id);
   }
 
   @SubscribeMessage('setInit')
   setInit(client: Socket, data) {
-    if (client.data.isInit) {
-      return;
+    if (!client.data.isInit) {
+      client.data.nickname = data.nickname ? data.nickname : `user-${client.id}`;
+      client.data.isInit = true;
     }
-
-    client.data.nickname = data.nickname ? data.nickname : `user-${client.id}`;
-    client.data.isInit = true;
 
     return {
       id: client.id,
       nickname: client.data.nickname,
       room: {
         roomId: 'room:lobby',
-        name: '로비'
+        roomName: '로비'
       }
     };
   }
 
-  @SubscribeMessage('draw')
-  handleDraw(@MessageBody() data: any) {
-    this.drawingService.handleDraw(data);
-    // this.server.emit('data', data);
+  @SubscribeMessage('getRoomList')
+  getRoomList(client: Socket) {
+    client.emit('getRoomList', this.drawingService.getRoomList());
+  }
+
+  @SubscribeMessage('createRoom')
+  createRoom(client: Socket, nickname: string, roomName: string) {
+    console.log(roomName);
+    if (client.data.roomId !== 'room:lobby' && this.server.sockets.adapter.rooms.get(client.data.roomId).size === 1) {
+      this.drawingService.deleteRoom(client.data.roomId);
+    }
+
+    client.data.nickname = nickname;
+    this.drawingService.createRoom(client, nickname, roomName);
+
+    return {
+      roomId: client.data.roomId,
+      roomName: this.drawingService.getRoom(client.data.roomId).roomName
+    };
+  }
+
+  @SubscribeMessage('enterRoom')
+  enterRoom(client: Socket, roomId: string) {
+    if (client.rooms.has(roomId)) {
+      return;
+    }
+
+    if (client.data.roomId !== 'room:lobby' && this.server.sockets.adapter.rooms.get(client.data.roomId).size === 1) {
+      this.drawingService.deleteRoom(client.data.roomId);
+    }
+
+    this.drawingService.deleteRoom(client.data.roomId);
+
+    return {
+      roomId: roomId,
+      roomName: this.drawingService.getRoom(roomId).roomName
+    };
+  }
+
+  @SubscribeMessage('setNickname')
+  setNickname(client: Socket, nickname: string) {
+    client.data.nickname = nickname;
+  }
+
+  @SubscribeMessage('sendDrawLines')
+  sendDrawLines(client: Socket, data: any) {
+    client.rooms.forEach(roomId => client.to(roomId).emit('getDrawLines', data));
   }
 
   @SubscribeMessage('sendMessage')
-  sendMessage(client: Socket, message: string): void {
+  sendMessage(client: Socket, message: string) {
     client.rooms.forEach(roomId =>
       client.to(roomId).emit('getMessage', {
         id: client.id,
--- src/drawing/drawing.service.ts
+++ src/drawing/drawing.service.ts
@@ -1,24 +1,75 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class DrawingService {
-  private clients: Socket[] = [];
-
-  handleConnection(client: Socket) {
-    this.clients.push(client);
-    client.leave(client.id);
-    client.data.roomId = 'room:lobby';
-    client.join('room:lobby');
+  private roomList;
+  constructor() {
+    this.roomList = {
+      'room:lobby': {
+        roomId: 'room:lobby',
+        roomName: '로비',
+        masterId: null
+      }
+    };
   }
 
-  handleDisconnect(client: Socket) {
-    this.clients = this.clients.filter(c => c !== client);
-  }
-
-  handleDraw(data: any) {
-    this.clients.forEach(client => {
-      client.emit('draw', data);
+  createRoom(client: Socket, nickname: string, roomName: string): void {
+    const roomId = `room:${uuidv4()}`;
+    client.emit('getMessage', {
+      id: null,
+      nickname: '안내',
+      message: `"${nickname}"님이 "${roomName}"방을 생성하였습니다.`
     });
+
+    this.roomList[roomId] = {
+      roomId,
+      roomName,
+      masterId: client.id
+    };
+
+    client.data.roomId = roomId;
+    client.rooms.clear();
+    client.join(roomId);
+  }
+
+  enterRoom(client: Socket, roomId: string) {
+    client.data.roomId = roomId;
+    client.rooms.clear();
+    client.join(roomId);
+
+    const { nickname } = client.data;
+    const { roomName } = this.getRoom(roomId);
+    client.to(roomId).emit('getMessage', {
+      id: null,
+      nickname: '안내',
+      message: `"${nickname}"님이 "${roomName}"방에 입장하셨습니다.`
+    });
+  }
+
+  exitRoom(client: Socket, roomId: string) {
+    client.data.roomId = `room:lobby`;
+    client.rooms.clear();
+    client.join(`room:lobby`);
+
+    const { nickname } = client.data;
+    client.to(roomId).emit('getMessage', {
+      id: null,
+      nickname: '안내',
+      message: `"${nickname}"님이 방에서 나갔습니다.`
+    });
+  }
+
+  getRoom(roomId: string) {
+    return this.roomList[roomId];
+  }
+
+  getRoomList() {
+    return this.roomList;
+  }
+
+  deleteRoom(roomId: string) {
+    delete this.roomList[roomId];
   }
 }
